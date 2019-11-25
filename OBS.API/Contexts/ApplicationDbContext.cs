b5dc61141f5d6450dea01cd6806159aa3fea02ba
R041910075 <akash.cse10@gmail.com>
2019-11-25 19:29:12 +0600

    DbContext setup done.

--- OBS.API/Contexts/ApplicationDbContext.cs
+++ OBS.API/Contexts/ApplicationDbContext.cs
@@ -1,8 +1,15 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using OBS.API.Model;
+using OBS.API.Model.Interfaces;
 
 namespace OBS.API.Contexts
 {
@@ -22,36 +29,164 @@ namespace OBS.API.Contexts
         public DbSet<Category> Categories { set; get; }
         public DbSet<BookCategory> BookCategories { get; set; }
         
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        protected override void OnModelCreating(ModelBuilder builder)
         {
+            foreach (var entity in builder.Model.GetEntityTypes())
+            {
+                if (typeof(ISoftDeletable).IsAssignableFrom(entity.ClrType) == true)
+                {
+                    entity.AddProperty(_isDeletedProperty, typeof(bool));
 
-            // Book and Category Many to Many Relationship
-            modelBuilder.Entity<BookCategory>()       // THIS IS FIRST
+                    builder
+                        .Entity(entity.ClrType)
+                        .HasQueryFilter(GetIsDeletedRestriction(entity.ClrType));
+                }
+            }
+
+            base.OnModelCreating(builder);
+
+            builder.Entity<ApplicationUser>(b =>
+            {
+                // Each User can have many UserClaims
+                b.HasMany(e => e.Claims)
+                    .WithOne()
+                    .HasForeignKey(uc => uc.UserId)
+                    .IsRequired();
+
+                // Each User can have many UserLogins
+                b.HasMany(e => e.Logins)
+                    .WithOne()
+                    .HasForeignKey(ul => ul.UserId)
+                    .IsRequired();
+
+                // Each User can have many UserTokens
+                b.HasMany(e => e.Tokens)
+                    .WithOne()
+                    .HasForeignKey(ut => ut.UserId)
+                    .IsRequired();
+
+                // Each User can have many entries in the UserRole join table
+                b.HasMany(e => e.UserRoles)
+                    .WithOne(e => e.User)
+                    .HasForeignKey(ur => ur.UserId)
+                    .IsRequired();
+            });
+
+            builder.Entity<ApplicationRole>(b =>
+            {
+                // Each Role can have many entries in the UserRole join table
+                b.HasMany(e => e.UserRoles)
+                    .WithOne(e => e.Role)
+                    .HasForeignKey(ur => ur.RoleId)
+                    .IsRequired();
+            });
+            
+            
+            builder.Entity<BookCategory>()       // THIS IS FIRST
                 .HasOne(u => u.Book).WithMany(u => u.BookCategories).IsRequired().OnDelete(DeleteBehavior.Restrict);
 
-            modelBuilder.Entity<BookCategory>()       // THIS IS FIRST
+            builder.Entity<BookCategory>()       // THIS IS FIRST
                 .HasOne(u => u.Category).WithMany(u => u.BookCategories).IsRequired().OnDelete(DeleteBehavior.Restrict);
 
-            modelBuilder.Entity<BookCategory>()
+            builder.Entity<BookCategory>()
                 .HasKey(bc => new { bc.BookId, bc.CategoryId });
-            modelBuilder.Entity<BookCategory>()
+            builder.Entity<BookCategory>()
                 .HasOne(bc => bc.Book)
                 .WithMany(b => b.BookCategories)
                 .HasForeignKey(bc => bc.BookId);
-            modelBuilder.Entity<BookCategory>()
+            builder.Entity<BookCategory>()
                 .HasOne(bc => bc.Category)
                 .WithMany(c => c.BookCategories)
                 .HasForeignKey(bc => bc.CategoryId);
 
-            modelBuilder.Entity<Book>()
+            builder.Entity<Book>()
                 .HasOne(e => e.Author)
                 .WithMany(e => e.Books)
                 .HasForeignKey(e => e.AuthorId)
                 .OnDelete(DeleteBehavior.Restrict);
+        }
 
-            base.OnModelCreating(modelBuilder);
 
+        public override int SaveChanges(bool acceptAllChangesOnSuccess)
+        {
+            OnBeforeSaving();
+            return base.SaveChanges(acceptAllChangesOnSuccess);
+        }
+
+        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            OnBeforeSaving();
+            return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
         }
         
+        private void OnBeforeSaving()
+        {
+            var entries = ChangeTracker.Entries();
+            foreach (var entry in entries)
+            {
+                if (entry.Entity is ITrackable trackable)
+                {
+                    var now = DateTime.UtcNow;
+                    var user = GetCurrentUser();
+                    switch (entry.State)
+                    {
+                        case EntityState.Modified:
+                            trackable.LastUpdatedAt = now;
+                            trackable.LastUpdatedBy = user;
+                            break;
+
+                        case EntityState.Added:
+                            trackable.CreatedAt = now;
+                            trackable.CreatedBy = user;
+                            trackable.LastUpdatedAt = now;
+                            trackable.LastUpdatedBy = user;
+                            entry.Property(_isDeletedProperty).CurrentValue = false;
+                            break;
+                        case EntityState.Deleted:
+                            entry.Property(_isDeletedProperty).CurrentValue = true;
+                            entry.State = EntityState.Modified;
+                            break;
+                    }
+                }
+            }
+        }
+        
+        private string GetCurrentUser()
+        {
+            var httpContext = _httpContextAccessor.HttpContext;
+            if (httpContext != null)
+            {
+                var authenticatedUserName = httpContext.User.Identity.Name;
+                return authenticatedUserName;
+            }
+
+            return null;
+        }
+        
+        private string GetCurrentUserId()
+        {
+            var httpContext = _httpContextAccessor.HttpContext;
+            if (httpContext != null)
+            {
+                var loginUser = httpContext.User.Claims.Where(c => c.Type == "sub")
+                    .Select(c => c.Value).SingleOrDefault();
+                return loginUser;
+            }
+
+            return null;
+        }
+        
+        private const string _isDeletedProperty = "IsDeleted";
+        
+        private static readonly MethodInfo _propertyMethod = typeof(EF).GetMethod(nameof(EF.Property), BindingFlags.Static | BindingFlags.Public).MakeGenericMethod(typeof(bool));
+
+        private static LambdaExpression GetIsDeletedRestriction(Type type)
+        {
+            var parm = Expression.Parameter(type, "it");
+            var prop = Expression.Call(_propertyMethod, parm, Expression.Constant(_isDeletedProperty));
+            var condition = Expression.MakeBinary(ExpressionType.Equal, prop, Expression.Constant(false));
+            var lambda = Expression.Lambda(condition, parm);
+            return lambda;
+        }
     }
 }
--- OBS.API/Model/ApplicationUser.cs
+++ OBS.API/Model/ApplicationUser.cs
@@ -1,9 +1,22 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
+using OBS.API.Model.Interfaces;
 
 namespace OBS.API.Model
 {
-    public class ApplicationUser: IdentityUser<long>
+    public class ApplicationUser : IdentityUser<long>, ITrackable
     {
-        public string Address { get; set; }
+        public string FullName { get; set; }
+        public long KeyMakerId { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTimeOffset LastUpdatedAt { get; set; }
+        public string LastUpdatedBy { get; set; }
+        public bool IsDeleted { get; set; }
+        public virtual ICollection<IdentityUserClaim<long>> Claims { get; set; }
+        public virtual ICollection<IdentityUserLogin<long>> Logins { get; set; }
+        public virtual ICollection<IdentityUserToken<long>> Tokens { get; set; }
+        public virtual ICollection<ApplicationUserRole> UserRoles { get; set; }
     }
 }
